15d5070f37122b4d8e545ce36add37ee7f60d1e0
Jhon Jairo <jjpotosi@iemsantateresita.edu.co>
2020-09-04 10:21:48 -0500

    activity agregada

--- app/src/main/java/com/example/siembraapp/IniciarSesion.java
+++ app/src/main/java/com/example/siembraapp/IniciarSesion.java
@@ -2,18 +2,13 @@ package com.example.siembraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class IniciarSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_iniciar_sesion);
     }
 }
