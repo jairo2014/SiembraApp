e21b3c87c0c68335984150306595f3b67a35b665
adccasas <adcasas@iemsantateresita.edu.co>
2020-09-15 11:35:41 -0500

    Clase siembra

--- app/src/main/java/Siembra.java
+++ app/src/main/java/Siembra.java
@@ -1,40 +1,13 @@
-public class Siembra {
+import androidx.room.PrimaryKey;
+
+public class Siembra{
 
 
-
-
-    private string mNombre;
-    private int mVariedadAbono;
+    @PrimaryKey rimary()
     private int mCodigoSiembra;
+    private Date mfechaSiembra;
     private float mArea;
     private int mCantidadAbono;
-    private Date mFechaSiembra;
-    private int mCantidadSemilla;
-    private int mVariedadSemilla;
-    private Date mClima;
-    public string getNombre(){
-        return mNombre;
-    }
-    public string getmNombre() {
-
-    }
-    public string getmNombre() {
-
-    }
-    public string getmNombre() {
-
-    }
-    public string getmNombre() {
-
-    }
-    public string getmNombre() {
-
-    }
-    public string getmNombre() {
-
-    }
-    public string getmNombre() {
-
-    }
+    private int mVariedadAbono;
 
 }
