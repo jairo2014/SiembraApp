e3fecbd9d21942a121dda603380c9956bd34a9c7
adccasas <adcasas@iemsantateresita.edu.co>
2020-09-15 16:35:52 -0500

    Clase siembra

--- app/src/main/java/Siembra.java
+++ app/src/main/java/Siembra.java
@@ -4,10 +4,15 @@ public class Siembra{
 
 
     @PrimaryKey rimary()
+
     private int mCodigoSiembra;
     private Date mfechaSiembra;
     private float mArea;
     private int mCantidadAbono;
     private int mVariedadAbono;
+
+}
+public string getNombre(){
+    return
 
 }
