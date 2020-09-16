9a75b23d13b8762553a8e25cc5031a51e13a1731
Jhon Jairo <jjpotosi@iemsantateresita.edu.co>
2020-09-16 09:42:39 -0500

    nuevos cambios

---
+++ app/src/main/java/Siembra.java
@@ -0,0 +1,40 @@
+public class Siembra {
+
+
+
+
+    private string mNombre;
+    private int mVariedadAbono;
+    private int mCodigoSiembra;
+    private float mArea;
+    private int mCantidadAbono;
+    private Date mFechaSiembra;
+    private int mCantidadSemilla;
+    private int mVariedadSemilla;
+    private Date mClima;
+    public string getNombre(){
+        return mNombre;
+    }
+    public string getmNombre() {
+
+    }
+    public string getmNombre() {
+
+    }
+    public string getmNombre() {
+
+    }
+    public string getmNombre() {
+
+    }
+    public string getmNombre() {
+
+    }
+    public string getmNombre() {
+
+    }
+    public string getmNombre() {
+
+    }
+
+}
