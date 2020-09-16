c01b6b5ab9ef978ef6d8c988b3d2629fc05261ce
Carlos Timarán <ctimaran@iemsantateresita.edu.co>
2020-09-16 15:37:09 -0500

    Revisada Clase Siembra y Module:app

--- app/src/main/java/Siembra.java
+++ app/src/main/java/Siembra.java
@@ -1,33 +1,52 @@
+
 import androidx.room.ColumnInfo;
+import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
 
+@Entity(tableName = "siembra")
 public class Siembra {
 
-    private String mNombre;
+    // Variables privadas solo se pueden acceder en el contexto de la clase
+    @PrimaryKey(autoGenerate = true)
     private int mCodigoSiembra;
-    private Date mfechaSiembra;
+
+    @ColumnInfo(name = "ubicacion")
+    private String mUbicacion;
+    @ColumnInfo(name = "variedad")
+    private String mVariedad;
+    @ColumnInfo(name = "fecha_siembra")
+    private Date mFechaSiembra;
+    @ColumnInfo(name = "area")
     private float mArea;
     private int mCantidadAbono;
     private int mVariedadAbono;
     private int mCantidadSemilla;
     private int mVariedadSemilla;
-    private Date mClima;
 
+    //Getter
+    public String getUbicacion(){
+        return mUbicacion;
+    }
+    public int getCantidadAbono(){
+        return mCantidadAbono;
+    };
 
-    public String getNombre(){return mNombre;}
-    public String getCantidad;
+    public Date getFechaSiembra(){
+        return mFechaSiembra;
+    };
 
-    public void getFechaSiembra;
+    public float getArea(){
+        return  mArea;
+    };
 
+    //Setter
+    public void setUbicacion(String ubicacion){
+        this.mUbicacion = ubicacion;
+    }
 
-    public String getNombre;
-
-    public String getNombre;
-
-    public String getNombre;
-
-
-
+    public void setCantidadAbono(int cantidadAbono){
+        this.mCantidadAbono = cantidadAbono;
+    };
 }
